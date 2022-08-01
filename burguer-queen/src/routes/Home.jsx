c6e668f98e727b47500045209bbeba5797323bc4
Clariveljn <cjeldres5@gmail.com>
2022-08-01 08:15:34 -0400

    template home

---
+++ burguer-queen/src/routes/Home.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import image from "../images/logoCoffe.png";
+import '../home.css'
+
+const Home = () => {
+  return (
+    <>
+      <div className="imagen">
+        <img src={image}/>
+      </div>
+
+      <form className='loginForm'>
+        <p className="title">¡Bienvenidos! </p >
+        <p className="subtitle">Esperamos que tengas un excelente turno de trabajo</p> 
+
+        <input 
+          class = "email" 
+          type="text" 
+          name='email'
+          placeholder="Usuario">
+        </input>
+        <input 
+          class = "password" 
+          type="password" 
+          name='password'
+          placeholder="Contraseña">
+        </input>
+        <button type = "submit" className="logIn">Iniciar Sesión</button>
+      </form>
+    </>
+  );
+}
+
+  export default Home;

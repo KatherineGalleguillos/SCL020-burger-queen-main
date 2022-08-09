ca9b0bb06cfc4ca413b9c614f835b9ae0e5a7496
katherine <galleguillos.cantuarias@gmail.com>
2022-08-09 10:32:49 -0400

    nuevo

---
+++ burguer-queen/src/Fb.jsx
@@ -0,0 +1,9 @@
+export const app = firebase.initializeApp({
+    "projectId": "tostadomiel-a1368",
+    "appId": "1:431109026588:web:cb43c0677ce2185a4080d9",
+    "storageBucket": "tostadomiel-a1368.appspot.com",
+    "locationId": "us-central",
+    "apiKey": "AIzaSyBYwBHNwK6y_F350_5P1O_a2lV2wdsk4Ck",
+    "authDomain": "tostadomiel-a1368.firebaseapp.com",
+    "messagingSenderId": "431109026588"
+  });

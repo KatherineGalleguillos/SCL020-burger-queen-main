b1a1316bc11a0cdedafceae6b8fe74400ac0df2a
Clariveljn <cjeldres5@gmail.com>
2022-08-10 11:57:45 -0400

    data

--- burger-queen/src/routes/TakeOrder.jsx
+++ burger-queen/src/routes/TakeOrder.jsx
@@ -1,11 +1,19 @@
  import Navbar from "../components/navbar";
 import image from "../images/logoCoffe.png";
+ import coffe from "../data.json/coffe.json"
+ import cake from "../data.json/cake.json"
+
+
+console.log(coffe);
+
 
 const TakeOrder = () => {
+  
     return (
       <>
           <Navbar></Navbar>
           <h1>Tomar Pedido</h1>
+
         
       </>
     );
